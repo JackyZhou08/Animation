b84c111e5a93822a53efb85194821c9af87404cf
Jacky <jackyzhou@ideaculture.com>
2017-08-15 18:05:10 +0800

    inital version

---
+++ app/src/main/java/com/jacky/animation/MainActivity.java
@@ -0,0 +1,18 @@
+package com.jacky.animation;
+
+import android.graphics.drawable.AnimationDrawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ImageView animationImg1 = (ImageView) findViewById(R.id.iv_animation);
+        AnimationDrawable animationDrawable1 = (AnimationDrawable) animationImg1.getDrawable();
+        animationDrawable1.start();
+    }
+}
